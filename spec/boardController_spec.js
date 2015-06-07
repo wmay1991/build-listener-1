6e6be2bfb5440b7f77e48c544c0810ce474b55e1
Don Abney <tdonaldabney@gmail.com>
2015-06-07 11:58:38 -0400

    Adding in the ability to write the status to the controller board

--- spec/boardController_spec.js
+++ spec/boardController_spec.js
@@ -1,14 +1,28 @@
 describe("BoardController", function() {
 
+  var boardController;
+
   describe("setBoardState()", function() {
 
+    beforeEach(function() {
+    });
+
     it("will set board status to all ON if unknown", function() {
+      boardController = new BoardController(); 
       var buildStatus = 'unknown';
-      var boardState = BoardController.setBoardState(buildStatus);
+      var boardState = boardController.setBoardState(buildStatus);
       expect(boardState.length).toEqual(3);
       expect(boardState[0]).toEqual(1);
       expect(boardState[1]).toEqual(1);
       expect(boardState[2]).toEqual(1);
+    });
+
+    it("will write the all ON state to the controller board when unknown", function() {
+      var mySpy = spyOn(BoardController.prototype, 'writeBoardState').and.callThrough(); 
+      boardController = new BoardController(); 
+      var buildStatus = 'unknown';
+      var boardState = boardController.setBoardState(buildStatus);
+      expect(mySpy).toHaveBeenCalledWith([1,1,1]);
     });
 
 });
--- src/boardController.js
+++ src/boardController.js
@@ -1,7 +1,20 @@
-var BoardController = BoardController || {};
+var BoardController = function(){};
 
-BoardController.setBoardState = function(inputParameter) {
-    return [1,1,1];
-};
+BoardController.prototype.setBoardState = function(inputParameter) {
+    var boardStatus = null;
+    switch (inputParameter) {
+        case 'unknown':
+          boardStatus = [1,1,1];
+          break;
+        default:
+          boardStatus = [0,0,0];
+    }
+    this.writeBoardState(boardStatus);
+    return boardStatus;
+}
+
+BoardController.prototype.writeBoardState = function (boardStatus) {
+    return null;
+}    
 
 module.exports= BoardController;
