6608dee8f92fe109a9d2303335cf881168b57e99
TIMOTHY ABNEY <donabney@Pillartechnologys-MacBook-Pro-2.local>
2015-07-17 18:31:14 -0400

    added in hardware write function

--- app/boardController.js
+++ app/boardController.js
@@ -1,13 +1,23 @@
+var mraa = require('mraa');
+
+var pin11 = new mraa.Gpio(11);
+var pin12 = new mraa.Gpio(12);
+var pin13 = new mraa.Gpio(13);
+
 var boardState = {};
 
 var BoardController = function(){
     this.initBoardState();
 };
 
+BoardController.prototype.getBoardState = function() {
+	return boardState;
+}
+
 BoardController.prototype.initBoardState = function() {
-    boardState['pin13'] = {value: 0};    
-    boardState['pin12'] = {value: 0};    
-    boardState['pin11'] = {value: 0};    
+    boardState['pin11'] = {pin: pin11, value: 0};    
+    boardState['pin12'] = {pin: pin12, value: 0};    
+    boardState['pin13'] = {pin: pin13, value: 0};    
 };
 
 BoardController.prototype.setBoardState = function(inputParameter) {
@@ -33,7 +43,9 @@ BoardController.prototype.setBoardState = function(inputParameter) {
 }
 
 BoardController.prototype.writeBoardState = function (boardState) {
-    return null;
+  for (var pinNum in boardState) {
+        boardState[pinNum].pin.write(boardState[pinNum].value);
+  }
 }    
 
 module.exports= BoardController;
--- spec/boardController_spec.js
+++ spec/boardController_spec.js
@@ -1,17 +1,24 @@
 var BoardController = require('../app/boardController');
+var mraa = require('mraa')
 
 describe("BoardController", function() {
 
   var boardController;
-  var allPinsOn
+  var allPinsOn;
+  var allPinsOff;
+
+  var pin11 = new mraa.Gpio(11);
+  var pin12 = new mraa.Gpio(12);
+  var pin13 = new mraa.Gpio(13);
 
   describe("setBoardState()", function() {
 
     beforeEach(function() {
-        allPinsOn = { pin13: { value: 1 }, pin12: { value: 1 }, pin11: { value: 1 } };
-        pin13On = { pin13: { value: 1 }, pin12: { value: 0 }, pin11: { value: 0 } };
-        pin12On = { pin13: { value: 0 }, pin12: { value: 1 }, pin11: { value: 0 } };
-        pin11On = { pin13: { value: 0 }, pin12: { value: 0 }, pin11: { value: 1 } };
+        allPinsOn = { pin11: { pin: pin11, value: 1 }, pin12: { pin: pin12, value: 1 }, pin13: { pin: pin13, value: 1 } };
+        allPinsOff = { pin11: { pin: pin11, value: 0 }, pin12: { pin: pin12, value: 0 }, pin13: { pin: pin13, value: 0 } };
+        pin13On = { pin11: { pin: pin11, value: 0 }, pin12: { pin: pin12, value: 0 }, pin13: { pin: pin13, value: 1 } };
+        pin12On = { pin11: { pin: pin11, value: 0 }, pin12: { pin: pin12, value: 1 }, pin13: { pin: pin13, value: 0 } };
+        pin11On = { pin11: { pin: pin11, value: 1 }, pin12: { pin: pin12, value: 0 }, pin13: { pin: pin13, value: 0 } };
     });
 
     it("will set board state to all ON if unknown", function() {
@@ -24,9 +31,15 @@ describe("BoardController", function() {
       expect(boardState['pin11'].value).toEqual(1);
     });
 
+    it("will write the all OFF state to the controller board when initialized", function() {
+      boardController = new BoardController();
+      boardController.initBoardState();
+      expect(boardController.getBoardState()).toEqual(allPinsOff);
+    });
+
     it("will write the all ON state to the controller board when unknown", function() {
-      var mySpy = spyOn(BoardController.prototype, 'writeBoardState').andCallThrough(); 
-      boardController = new BoardController(); 
+      var mySpy = spyOn(BoardController.prototype, 'writeBoardState').andCallThrough();
+      boardController = new BoardController();
       var buildStatus = 'unknown';
       var boardState = boardController.setBoardState(buildStatus);
       expect(mySpy).toHaveBeenCalledWith(allPinsOn);
@@ -53,6 +66,25 @@ describe("BoardController", function() {
       expect(boardState).toEqual(pin11On);
     });
 
-});
+    it("will write board state to pin 11 ON if fail", function() {
+      boardController = new BoardController(); 
+      var mySpy = spyOn(pin11, 'write');
+      boardController.writeBoardState(pin11On);
+      expect(mySpy).toHaveBeenCalledWith(1);
+    });
 
+    it("will write board state to pin 12 ON if warn", function() {
+      boardController = new BoardController();
+      var mySpy = spyOn(pin12, 'write');
+      boardController.writeBoardState(pin12On);
+      expect(mySpy).toHaveBeenCalledWith(1);
+    });
+
+    it("will write board state to pin 13 ON if pass", function() {
+      boardController = new BoardController();
+      var mySpy = spyOn(pin13, 'write');
+      boardController.writeBoardState(pin13On);
+      expect(mySpy).toHaveBeenCalledWith(1);
+    });
+  });
 });
